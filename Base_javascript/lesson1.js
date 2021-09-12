4bc0632b380acd2ac6d638c6ae21b6cda2936187
Nikita <lomello@yandex.ru>
2021-09-12 15:12:20 +0300

    lesson1

--- Base_javascript/lesson1.js
+++ Base_javascript/lesson1.js
@@ -6,21 +6,38 @@ const products = [
     {id: 3, title: 'Keyboard', price: 250},
     {id: 4, title: 'Gamepad', price: 150}
 ]
-// console.log(products)
 
-
-const renderProduct = (title, price) =>{
-    return `<div class='product-item'>
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="by-btn">Add</button>
-            </div>`
-}
-const renderProducts = (list) => {
-    const productList = list.map((item) =>{
-        return renderProduct(item.title, item.price);
+let btn = document.querySelector('.by-btn')
+btn.addEventListener('click',btnClick)
+function btnClick() {
+    products.forEach(function (elem, i){
+        document.body.insertAdjacentHTML('beforeend',`
+        <div class='product-item'>
+                 <h3>${products[i].title}</h3>
+                 <p>${products[i].price}</p>
+                 <button class="by-btn">Add</button>
+             </div>
+        `)
     })
-    // console.log(productList)
-    document.querySelector('.products').innerHTML = productList;
+    btn.removeEventListener('click',btnClick)
 }
-renderProducts(products)
+
+// let btn = document.querySelector('.by-btn')
+
+
+
+
+
+
+
+
+
+
+
+// const renderProduct = (title, price) =>{
+//     return `<div class='product-item'>
+//                 <h3>${title}</h3>
+//                 <p>${price}</p>
+//                 <button class="by-btn">Add</button>
+//             </div>`
+// }
